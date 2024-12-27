3df131585f474cb9489c003e2b263f365d96efcd
Seitek2002 <seitekbegaliev@gmail.com>
2024-12-27 14:38:40 +0600

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const swiper = new Swiper('.swiper', {
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-
-  on: {
-    autoplayTimeLeft(s, time, progress) {
-      const progressBar = document.querySelector(".swiper-pagination-bullet-active .progress-bar");
-      progressBar.style.width = progress * 100 + "%";
-    }
-  },
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return `<span class="${className}"> <div class="progress-bar"></div> </span>`;
-    },
-  },
-});
-
 const btn = document.querySelector(".footer__content button");
 const result = document.querySelector(".toast");
 
