3aed7e9efd137e848ff99ea4d16b0cc49a61de27
Seitek2002 <seitekbegaliev@gmail.com>
2024-12-27 17:20:59 +0600

    added-actions

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector(".footer__content button");
+const btnI = document.querySelector(".button-i");
+const btnR = document.querySelector(".button-r");
 const result = document.querySelector(".toast");
 
-const getData = async () => {
+const getData = async (endpoint) => {
   result.style.right = 0;
   result.innerText = "Loading....";
-  await fetch("http://127.0.0.1:1010/dispense")
+  await fetch("http://127.0.0.1:1010/" + endpoint)
     .then((res) => res.json())
     .then((data) => {
       result.innerText = "Карта успешно выдана!";
@@ -19,4 +21,6 @@ const getData = async () => {
     }, 3000)
 };
 
-btn.addEventListener("click", getData);
+btn.addEventListener("click", () => getData('dispense'));
+btnI.addEventListener("click", () => getData('init'));
+btnR.addEventListener("click", () => getData('reset'));
