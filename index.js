bb66e68fcd6d6cf46717c8587b235c0946f6fa4f
Seitek2002 <seitekbegaliev@gmail.com>
2024-12-27 20:01:25 +0600

    animation-add

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ const btn = document.querySelector(".footer__content button");
 const btnI = document.querySelector(".button-i");
 const btnR = document.querySelector(".button-r");
 const result = document.querySelector(".toast");
+const footerResult = document.querySelector(".footer__content");
 
-const getData = async (endpoint) => {
+btn.addEventListener("click", async () => {
   result.style.right = 0;
   result.innerText = "Loading....";
-  await fetch("http://127.0.0.1:1010/" + endpoint)
+  await fetch("http://127.0.0.1:1010/dispense")
     .then((res) => res.json())
     .then((data) => {
       result.innerText = "Карта успешно выдана!";
+      footerResult.classList.add("active");
+    })
+    .catch((error) => {
+      result.style.background = "red";
+      result.innerText = "Неизвестная ошибка!";
+      footerResult.classList.add("active");
+    });
+
+    setTimeout(() => {
+      result.style.right = -100 + "%";
+      footerResult.classList.remove("active");
+    }, 3000)
+});
+
+btnI.addEventListener("click", async () => {
+  result.style.right = 0;
+  result.innerText = "Loading....";
+  await fetch("http://127.0.0.1:1010/init")
+    .then((res) => res.json())
+    .then((data) => {
+      result.innerText = "Выполнена инициация!";
     })
     .catch((error) => {
       result.style.background = "red";
@@ -19,8 +41,22 @@ const getData = async (endpoint) => {
     setTimeout(() => {
       result.style.right = -100 + "%";
     }, 3000)
-};
+});
 
-btn.addEventListener("click", () => getData('dispense'));
-btnI.addEventListener("click", () => getData('init'));
-btnR.addEventListener("click", () => getData('reset'));
+btnR.addEventListener("click", async () => {
+  result.style.right = 0;
+  result.innerText = "Loading....";
+  await fetch("http://127.0.0.1:1010/reset")
+    .then((res) => res.json())
+    .then((data) => {
+      result.innerText = "Выполнен сброс!";
+    })
+    .catch((error) => {
+      result.style.background = "red";
+      result.innerText = "Неизвестная ошибка!";
+    });
+
+    setTimeout(() => {
+      result.style.right = -100 + "%";
+    }, 3000)
+});
